c41662579c54ec1679f82473650b55045e10a09a
Liene31 <lienebare@gmail.com>
2022-04-23 00:28:53 +0200

    Added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
   return (
@@ -28,15 +29,9 @@ export default function Weather() {
 
           <div className="col description">
             <ul>
-              <li>
-                <span>Sunny</span>
-              </li>
-              <li>
-                Humidity: <span>20%</span>
-              </li>
-              <li>
-                Wind: <span>5 km/h</span>
-              </li>
+              <li>Sunny</li>
+              <li>Humidity: 20%</li>
+              <li>Wind: 5 km/h</li>
             </ul>
           </div>
         </div>
