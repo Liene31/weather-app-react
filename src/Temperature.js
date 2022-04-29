120ce66aac1205c4b4132c53fae45373cb862eb9
Liene31 <lienebare@gmail.com>
2022-04-29 23:35:09 +0200

    Removed Fahrenheit

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,44 +1,10 @@
 import React, { useState } from "react";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function changeToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function changeToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div>
-        <span>{props.celsius}</span>
-        <span className="units">
-          {" "}
-          °C |{" "}
-          <a href="/" onClick={changeToFahrenheit}>
-            °F
-          </a>{" "}
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = Math.round((props.celsius * 9) / 5 + 32);
-    return (
-      <div>
-        <span>{fahrenheit}</span>
-        <span className="units">
-          {" "}
-          <a href="/" onClick={changeToCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <span>{props.celsius}</span>
+      <span className="units">°C</span>
+    </div>
+  );
 }
